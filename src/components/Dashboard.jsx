83eebf24af9f84b5e826b103189753df716468ff
Salhi Rayen <rayen.rsalhi9@gmail.com>
2025-09-05 12:08:38 +0100

    refactor(Dashboard): improve error handling in fetchMetrics function
    Add try-catch block to handle errors from supabase query and pass metrics data to state

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import supabase from '../supabase-client'
 
@@ -15,22 +16,31 @@ async function getHighestDeal() {
     console.log(response)
 }
 
-async function fetchMetrics() {
-    const response = await supabase
-        .from('sales_deals')
-        .select(
-            `
-            name,
-            value.sum()
-            `,
-        )
-    console.log(response)
+async function fetchMetrics(setMetrics) {
+    try {
+        const {error, data} = await supabase
+            .from('sales_deals')
+            .select(
+                `
+                name,
+                value.sum()
+                `,
+            )
+        if (error) throw error
+        setMetrics(data)
+    } catch(err) {
+        console.log(err)
+        throw err
+    }
 }
 
 export default function Dashboard() {
 
+    const [metrics, setMetrics] = React.useState([])
+    console.log(metrics)
+
     React.useEffect(() => {
-        fetchMetrics()
+        fetchMetrics(setMetrics)
     }, [])
 
     return (
