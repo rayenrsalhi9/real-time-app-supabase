5f572d03435f6b7514419336a7c84d73bfecb05e
Salhi Rayen <rayen.rsalhi9@gmail.com>
2025-09-05 11:47:24 +0100

    feat(Dashboard): add sales metrics fetching functionality
    Implement data fetching from supabase to display sales metrics in Dashboard component. The changes include renaming the supabase client variable for clarity and adding a fetchMetrics function with React useEffect hook.

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,9 +1,29 @@
+import React from 'react'
+import supabase from '../supabase-client'
 
+async function fetchMetrics() {
+    const response = await supabase
+        .from('sales_deals')
+        .select(
+            `
+            name,
+            value
+            `,
+        )
+        .order('value', { ascending: false })
+        .limit(1)
+    console.log(response)
+}
 
 export default function Dashboard() {
-  return (
-    <main>
-        <h2 className="title">Total Sales This Quarter ($)</h2>
-    </main>
-  )
+
+    React.useEffect(() => {
+        fetchMetrics()
+    }, [])
+
+    return (
+        <main>
+            <h2 className="title">Total Sales This Quarter ($)</h2>
+        </main>
+    )
 }
--- src/supabase-client.js
+++ src/supabase-client.js
@@ -3,6 +3,6 @@ import { createClient } from "@supabase/supabase-js";
 const sbUrl = import.meta.env.VITE_SB_URL
 const sbKey = import.meta.env.VITE_SB_KEY
 
-const client = createClient(sbUrl, sbKey)
+const supabase = createClient(sbUrl, sbKey)
 
-export default client
+export default supabase
