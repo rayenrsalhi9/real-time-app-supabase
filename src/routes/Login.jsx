b5c9ae13c276e5a5b4f9b94bfe350cd7a2f00cc8
Salhi Rayen <rayen.rsalhi9@gmail.com>
2025-09-06 11:58:09 +0100

    feat(login): improve login form styling and submission state
    - Add styling for form title and disabled submit button
    - Make form fields required and update button text during submission
    - Add form replacement to prevent duplicate submissions

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-refresh/only-export-components */
-import { Form, Link } from "react-router-dom"
+import { Form, Link, useNavigation } from "react-router-dom"
 
 export async function action({request}) {
   const formData = await request.formData()
@@ -11,24 +11,28 @@ export async function action({request}) {
 }
 
 export default function Login() {
+
+  const {state} = useNavigation()
+
   return (
     <>
       <header className='login-header'>
         <h1>Paper like a boss</h1>
       </header>
-      <Form className="auth-form" method="post">
+      <Form className="auth-form" method="post" replace>
         <div className="form-content">
+          <h2>Sign in</h2>
           <div className="form-field">
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" name="email" placeholder="example@domain.com" />
+            <input type="email" id="email" name="email" placeholder="example@domain.com" required />
           </div>
           <div className="form-field">
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" name="password" placeholder="********" />
+            <input type="password" id="password" name="password" placeholder="********" required />
           </div>
         </div>
-        <button className="submit-btn" type="submit">
-          Login
+        <button className="submit-btn" type="submit" disabled={state === 'submitting'}>
+          { state === 'submitting' ? "Signing in..." : "Sign in" }
         </button>
         <p className="signup-cta">
           Don't have an account? <Link to="/signup">Signup</Link>
